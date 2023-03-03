d62e3f2d5c3bd58112b0b061972c1990295521bd
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-03-03 10:19:47 +0100

    Add Vehicle and Booking Migration

---
+++ db/migrate/20230303090505_create_vehicles.rb
@@ -0,0 +1,13 @@
+class CreateVehicles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vehicles do |t|
+      t.string :name
+      t.string :image
+      t.string :model
+      t.decimal :daily_price
+      t.text :description
+      t.boolean :available, default: true
+      t.timestamps
+    end
+  end
+end
