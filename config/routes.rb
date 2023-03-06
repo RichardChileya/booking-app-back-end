65980c2e69935bedfb15e5f986540f4e048edda8
RichardChileya <richardchileyawk@gmail.com>
2023-03-06 15:48:10 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  # root "users#index"
 
   namespace :api do 
-    resources only: [:index, :show] do
+    resources :users, only: [:index, :show]
 
     end
   end
