5b7c07707debbc4e0788da247c1b3fa1248239ab
tashisrael <tashisrael04@gmail.com>
2023-03-06 15:59:20 +0300

    declare variables to use to test vehicle model

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
+	before :each do
+		user = User.create(name:'Richard', email:'rico@gmail.com', password:'1234567', role:1)
+		@vehicles = Vehicle.create(name:'Hilux', image:'https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg
+', model:'2021', daily_price:'300.10', description:'neat hilux with low gas consuption
+', available: true, user_id: 1)
+
+		
+	end
   
 end
