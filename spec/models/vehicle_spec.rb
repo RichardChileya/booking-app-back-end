fe7c318748667de1a52f3c06b54aae365b7153ae
tashisrael <tashisrael04@gmail.com>
2023-03-06 16:47:20 +0300

    add model tests for vehicles

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-	before :each do
-		@user = User.create(name:'Richard', email:'rico@gmail.com', password:'1234567', role:1)
-		@vehicles = Vehicle.create(name:'Hilux', image:'https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg', model:'2021', daily_price:'300.10', description:'neat hilux with low gas consuption', available: true, user_id: 1)
-	end
+  before :each do
+    @user = User.create(name: 'Richard', email: 'rico@gmail.com', password: '1234567', role: 1)
+    @vehicles = Vehicle.create(name: 'Hilux', image: 'https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg', model: '2021', daily_price: '300.10',
+                               description: 'neat hilux with low gas consuption', available: true, user_id: 1)
+  end
 
-	context 'Test for name availability' do
-		it 'name should be present' do
-			expect(@vehicles.name).to eq('Hilux')	
-		end	
+  context 'Test for name availability' do
+    it 'name should be present' do
+      expect(@vehicles.name).to eq('Hilux')
+    end
 
-		it 'image should be present' do
-			expect(@vehicles.image).to eq('https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg')	
-		end	
+    it 'image should be present' do
+      expect(@vehicles.image).to eq('https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg')
+    end
 
-		it 'model should be present' do
-			expect(@vehicles.model).to eq('2021')	
-		end
+    it 'model should be present' do
+      expect(@vehicles.model).to eq('2021')
+    end
 
-		it 'daily_price should be present' do
-			expect(@vehicles.daily_price).to eql(300.10)	
-		end	
+    it 'daily_price should be present' do
+      expect(@vehicles.daily_price).to eql(300.10)
+    end
 
-		it 'description should be present' do
-			expect(@vehicles.description).to eq('neat hilux with low gas consuption')	
-		end
-	end
-  
+    it 'description should be present' do
+      expect(@vehicles.description).to eq('neat hilux with low gas consuption')
+    end
+  end
 end
