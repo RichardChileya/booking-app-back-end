b54e4cd8d2db4fda738de21bd2ba4269fdae1c37
tashisrael <tashisrael04@gmail.com>
2023-03-06 16:44:57 +0300

    add description test

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Vehicle, type: :model do
 	before :each do
 		@user = User.create(name:'Richard', email:'rico@gmail.com', password:'1234567', role:1)
-		@vehicles = Vehicle.create(name:'Hilux', image:'https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg', model:'2021', daily_price:'300.10', description:'neat hilux with low gas consuption
-', available: true, user_id: 1)
+		@vehicles = Vehicle.create(name:'Hilux', image:'https://www.shutterstock.com/image-photo/kota-kinabalu-malaysia-jan-1st-260nw-559327324.jpg', model:'2021', daily_price:'300.10', description:'neat hilux with low gas consuption', available: true, user_id: 1)
 	end
 
 	context 'Test for name availability' do
@@ -21,8 +20,12 @@ RSpec.describe Vehicle, type: :model do
 		end
 
 		it 'daily_price should be present' do
-			expect(@vehicles.daily_price).to eq('300.10')	
+			expect(@vehicles.daily_price).to eql(300.10)	
 		end	
+
+		it 'description should be present' do
+			expect(@vehicles.description).to eq('neat hilux with low gas consuption')	
+		end
 	end
   
 end
