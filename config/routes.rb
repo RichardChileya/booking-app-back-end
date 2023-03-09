4fd2633799b404eec8c8894769937506f24e59ca
RichardChileya <richardchileyawk@gmail.com>
2023-03-09 15:27:49 +0200

    Add jwt authentication and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }
+  devise_for :users,
+  controllers: { sessions: 'users/sessions',
+    registrations: 'users/registrations'}
+    get '/api/users/member-data', to: 'members#show'
+  #  defaults: { format: :json }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
