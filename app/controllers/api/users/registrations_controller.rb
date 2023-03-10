a623c3395546a5122c83987c4815c74609d1174c
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-03-10 11:01:43 +0100

    Fix Rubocop error

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -14,6 +14,6 @@ class Api::Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_failed
-    render json: { message: "Something went wrong." }
+    render json: { message: 'Something went wrong.' }
   end
 end
