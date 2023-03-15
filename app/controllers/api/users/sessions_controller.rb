580bca94254474669f1c92b1f5cc45b19a4818bb
RichardChileya <richardchileyawk@gmail.com>
2023-03-15 18:16:06 +0200

    Add login success method

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -4,11 +4,30 @@ class Api::Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    login_success && return if resource.persisted?
+
+
+    # login_failure
   end
 
+  def login_success
+    render json: {
+      status: '00',
+      message: 'Logged in successfully'
+      # data: UserSerializer.new(resource).serializable_hash[:data]
+      # [:attributes]
+    }
+  end
+
+  def login_failure
+    render json: {
+      status: '01',
+      message: 'Login failure, try again!'
+    }
+  end
+  
   def respond_to_on_destroy
-    log_out_success && return if current_user
+    log_out_success && return if current_api_user
 
     log_out_failure
   end
