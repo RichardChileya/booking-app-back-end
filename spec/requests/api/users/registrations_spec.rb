4536ccef53468b98e4bb99914a56a1801825213b
recillah khamala <recillahk@gmail.com>
2023-03-18 01:35:01 +0300

    Add test for registration controller

--- spec/requests/api/users/registrations_spec.rb
+++ spec/requests/api/users/registrations_spec.rb
@@ -1,36 +1,17 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::Users::Registrations', type: :request do
+RSpec.describe 'api/users/registrations', type: :request do
   path '/api/signup' do
-    post('Sign Up a new User') do
-      tags 'Sign up'
-      description 'Sign up a new user'
-      consumes 'application/json'
-      produces 'application/json'
-      parameter name: :user, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          email: { type: :string },
-          password: { type: :string },
-          password_confirmation: { type: :string }
-        },
-        required: %w[name email password password_confirmation]
-      }
-      let(:user) do
-        {
-          user: {
-            name: 'Jes',
-            email: 'jes@gmail.com',
-            password: 'password',
-            password_confirmation: 'password'
-          }
-        }
-      end
-
+    post('create registration') do
       response(200, 'successful') do
-        examples 'application/json' =>
-          { message: 'Sign up successful' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
         run_test!
       end
     end
